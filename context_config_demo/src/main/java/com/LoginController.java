3f6b5c266b2f08f3a9c04024a9ae589bf21f0151
Vikas Panwar <cs.panwar@gmail.com>
2021-12-18 21:10:24 +0530

    Servlet Context Config and RequestDispatcher

---
+++ context_config_demo/src/main/java/com/LoginController.java
@@ -0,0 +1,50 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginController
+ */
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		RequestDispatcher rd = null;
+		if (username.equalsIgnoreCase("tom") && password.equals("jerry")) {
+			rd = request.getRequestDispatcher("SuccessServlet");
+			rd.forward(request, response);
+		}
+		else {
+			rd = request.getRequestDispatcher("login.html");
+			PrintWriter out = response.getWriter();
+			rd.include(request, response);			
+			out.println("<center><span style='color:red'>Invalid Credentials!!</span></center>");
+			out.close();
+		}
+	}
+
+}
---
+++ context_config_demo/src/main/java/com/MyServlet.java
@@ -0,0 +1,61 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ * 
+ */
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public MyServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	/*
+	 * protected void service(HttpServletRequest request, HttpServletResponse
+	 * response) throws ServletException, IOException { PrintWriter out =
+	 * response.getWriter(); out.print("<h1>Hello World, Service Method</h1>");
+	 * out.close(); }
+	 */
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("fname");
+		out.print("Hello World, doGet Method");
+		out.print("<br> Hello " + name);
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("fname");
+		out.print("Hello World, doPost Method");
+		out.print("<br> Hello " + name);
+		out.close();
+	}
+}
